0e419e37649a1dba89f6669df36164edc04df974
PrajnaKV123 <prajnakv584@gmail.com>
2025-11-20 16:30:01 +0530

    prajna

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,50 +3,17 @@ import Link from 'next/link';
 import Map from "../components/map";
 import Open from "../components/open";
 import Footer from "../components/footer"
+import Header from "../components/header"
 export default function Contact(){
     return(
         <div>
-            <div className="bg-black text-white h-30">
-        <div className="bg-[#c7a268] text-sm flex flex-col md:flex-row 
-        md:justify-between items-center
-         px-4 md:px-8 py-2 space-y-1 md:space-y-0">
-          <div className="text-center md:text-left">
-            Phone: <span className="font-medium">+12345678</span> |{" "}
-            <span className="font-medium">Email : food@restan.com</span>
-          </div>
-          <div>English</div>
-        </div>
-
-        <header
-          className="flex justify-between items-center px-10 py-4
-       bg-black border-b border-gray-800"
-        >
-          <h1 className="w-20">
-            <img
-              src={
-                "https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Flogo-light.png&w=750&q=75"
-              }
-            />
-          </h1>
-          <nav className="space-x-20 hidden md:block">
-            <Link href="/" className="hover:text-amber-400"> Home </Link>
-            <Link href="/about" className="hover:text-amber-400"> Pages </Link>
-            <Link href="/blog" className="hover:text-amber-400">  Blog  </Link>
-            <Link href="/contact" className="hover:text-amber-400">  Contact</Link>
-          </nav>
-          <button
-            className="border hover:text-amber-400 px-4 py-2  rounded "
-          >
-            Reservation
-          </button>
-        </header>
-        </div>
-    <div>
+          <Header/>
+          <div>
       <img className="w-full h-70 md:h-100 opacity-85" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS70qOalgPKlLRqby8tBn_FDoukfqSjmUO_kg&s"/>
       <p className="absolute inset-0 ml-23 md:ml-150 mt-60 md:mt-60 text-4xl md:text-6xl font-serif text-white">Contact Us</p>
-      <p className="absolute inset-0 text-white ml-30 md:ml-170 mt-70 md:mt-78">Home contact</p>
+      <p className="absolute inset-0 text-white ml-30 md:ml-170 mt-70 md:mt-78 ">Home contact</p>
     </div>
-{/*next section*/}
+          
     <div className="flex flex-wrap mt-30">
       <div className="dark:bg-white h-70 w-full md:w-90 border-1 md:ml-25 rounded-2xl">
         <img className="ml-33 mt-3 h-25 w-25"src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Ficon%2Fphone.png&w=640&q=75"/>
